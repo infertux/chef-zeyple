b404f00be6de67c34d068f7f67d1217715360aaf
Cédric Félizard <cedric@felizard.fr>
2015-06-14 12:58:59 +0000

    Fix bad version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cedric@felizard.fr'
 license          'MIT'
 description      'Installs/Configures Zeyple'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.0'
+version          '1.0.0'
 
 supports 'centos'
 supports 'debian'
